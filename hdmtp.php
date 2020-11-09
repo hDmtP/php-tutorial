a1e3d24ab4286949cfb68e932b20a8802dfcdd71
Pritam Dhara <pritamdhara914@gmail.com>
2020-11-09 20:39:54 +0530

    v1.0.0

---
+++ hdmtp.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo "Welcome to ";?>PHP Tutorial</title>
+</head>
+<body>
+<h1>
+<?php
+echo "this is hdmtp.php1\n";
+?>
+</h1>  
+<?php
+echo "this is hdmtp.php2\n";
+?>
+<?php
+echo "this is hdmtp.php3\n";
+?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php echo
+"Hello World\n<br>";
+echo 69+420;
+?>
+
+<?php
+$name = "hDmtP 645";
+$game = "Call of Duty";
+echo "A yt ch named $name is famous for playing $game<br>";
+?>
